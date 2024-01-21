3933464e6c288635c7fb54ca6b48a8e0025cf1ae
levyathan <sritharkalimuthu@gmail.com>
2024-01-21 16:28:51 +0530

    created list.js

--- Amazon_Books/src/index.js
+++ Amazon_Books/src/index.js
@@ -1,34 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-// import Book from './book';
+import Book from './book';
 import './style.css';
-
-// Creating array of booklist.
-const books =[
-        {
-        title :"Atomic-Habits",
-        author:"James Clear",
-        img:'./image/atomichabits.jpg'
-    },
-    {
-        title :"The Power of Your Subconsious Mind",
-        author:" Joseph Murphy",
-        img:'./image/subconsiousMind.jpg'
-    },
-    {
-        title :"Ikigai",
-        author:" Francesc Miralles & Hector Garcia",
-        img:'./image/ikigai.jpg'
-    }
-];
+import books from './list';
 
 function Bestsales(){
     return <section className="book-shelf">
     { /* Accessing item from the array of books using map*/ }
         {books.map((books)=>{
-          return <div className='book-column'><img src={books.img} alt='book'/>
-          <p id='title'>{books.title}</p>
-          <p id='author'>{books.author}</p></div>
+            const {img,title,author}=books;
+            {/*Passing destructed map items to the book component as props*/}
+          return <Book img={img} title={title} author={author}/>
         })}
     </section>
 }
---
+++ Amazon_Books/src/list.js
@@ -0,0 +1,20 @@
+// Creating array of booklist.
+const books =[
+    {
+    title :"Atomic-Habits",
+    author:"James Clear",
+    img:'./image/atomichabits.jpg'
+},
+{
+    title :"The Power of Your Subconsious Mind",
+    author:" Joseph Murphy",
+    img:'./image/subconsiousMind.jpg'
+},
+{
+    title :"Ikigai",
+    author:" Francesc Miralles & Hector Garcia",
+    img:'./image/ikigai.jpg'
+}
+];
+
+export default books;
