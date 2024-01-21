8ef92ee05f58a9f5df83f1a41ddd089182991ba0
levyathan <sritharkalimuthu@gmail.com>
2024-01-21 16:21:04 +0530

    Array map

--- Amazon_Books/src/index.js
+++ Amazon_Books/src/index.js
@@ -1,32 +1,36 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import Book from './book';
+// import Book from './book';
 import './style.css';
 
-const book ={
-    title :"Atomic-Habits",
-    author:"James Clear",
-    img:'./image/atomichabits.jpg'
-  }
-
-const book2={
-    title :"The Power of Your Subconsious Mind",
-    author:" Joseph Murphy",
-    img:'./image/subconsiousMind.jpg'
-}
-const book3={
-    title :"Ikigai",
-    author:" Francesc Miralles & Hector Garcia",
-    img:'./image/ikigai.jpg'
-}
+// Creating array of booklist.
+const books =[
+        {
+        title :"Atomic-Habits",
+        author:"James Clear",
+        img:'./image/atomichabits.jpg'
+    },
+    {
+        title :"The Power of Your Subconsious Mind",
+        author:" Joseph Murphy",
+        img:'./image/subconsiousMind.jpg'
+    },
+    {
+        title :"Ikigai",
+        author:" Francesc Miralles & Hector Garcia",
+        img:'./image/ikigai.jpg'
+    }
+];
 
 function Bestsales(){
-    return <div className="book-shelf">
-    { /* Passing props to the <Book/> */ }
-    <Book img={book.img} title={book.title} author={book.author}>out of stock</Book>
-    <Book img={book2.img} title={book2.title} author={book2.author}/>
-    <Book img={book3.img} title={book3.title} author={book3.author}/>
-    </div>
+    return <section className="book-shelf">
+    { /* Accessing item from the array of books using map*/ }
+        {books.map((books)=>{
+          return <div className='book-column'><img src={books.img} alt='book'/>
+          <p id='title'>{books.title}</p>
+          <p id='author'>{books.author}</p></div>
+        })}
+    </section>
 }
 
 function App(){
