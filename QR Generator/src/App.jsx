fb93c2daf80fc92bc7504b64ccdda5c9c9ab4b23
levyathan <sritharkalimuthu@gmail.com>
2024-02-11 23:21:18 +0530

    using spread operator

--- QR Generator/src/App.jsx
+++ QR Generator/src/App.jsx
@@ -3,13 +3,23 @@ import './App.css'
 
 function App() {
   const [image,setImage] = useState('');
-  const [data,setData] = useState('');
-  const [size,setSize]=useState(150);
+  const [dataobject,setDataobject] = useState({data:'',size:150});
+  const [error,setError] = useState('');
+
+  const handleinput = (e)=>{
+    setDataobject({...dataobject,[e.target.name]:e.target.value});
+  }
 
   function generatecode(){
     try{
-    const code = `https://api.qrserver.com/v1/create-qr-code/?size=${size}x${size}&data="${encodeURIComponent(data)}"`;
-    setImage(code);
+      if(dataobject.data === ''){
+        setImage('')
+        setError('Enter data to get QR code !!!');
+      }else{
+        setError('');
+        const code = `https://api.qrserver.com/v1/create-qr-code/?size=${dataobject.size}x${dataobject.size}&data=${encodeURIComponent(dataobject.data)}`;
+        setImage(code);
+      }
     }catch(error){
       console.log('Error while generating QR code',error)
     }
@@ -34,11 +44,12 @@ function App() {
     <section className='container'>
       <h1 className='title'>qr generator</h1>
       {image && <img src={image} className='qr-img'/>}
+      <p className='error-message'>{error}</p>
       <section>
           <label htmlFor='text-field' className='input-label'>Data for qr code : </label>
-          <input type='text' placeholder='Enter Data for QR code' id='text-field' onChange={(e)=>setData(e.target.value)}/>
+          <input type='text' placeholder='Enter Data for QR code' id='text-field' onChange={handleinput} name='data'/>
           <label htmlFor='size-field' className='input-label'>Image Size(e.g. 150) : </label>
-          <input type='text' placeholder='Enter Size for QR code' id='size-field' onChange={(e)=>setSize(e.target.value)}/>
+          <input type='text' placeholder='Enter Size for QR code' id='size-field' onChange={handleinput} name='size'/>
           <button className='generate-btn' onClick={generatecode}>Generate Code</button>
           <button className='download-btn' onClick={downloadcode}>Download Code</button>
       </section>
